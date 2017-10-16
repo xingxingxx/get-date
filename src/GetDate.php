0ca2dddff3b6fdb645803986805f8fd15c98f083
xiaoxingping <xx9815@qq.com>
2017-10-16 15:41:31 +0800

    项目初始化

---
+++ src/GetDate.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaoxingping
+ * Date: 2017/10/16
+ * Time: 15:40
+ */
+
+namespace xingxingxx;
+
+
+class GetDate
+{
+    public function getDate()
+    {
+        return time();
+    }
+}
